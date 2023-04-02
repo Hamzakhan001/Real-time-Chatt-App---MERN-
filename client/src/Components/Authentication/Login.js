d58490c8712f126c9168c932ff9485a7819e39a4
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-04-02 21:15:18 +0500

    login complete

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -1,10 +1,15 @@
 import React,{useState} from 'react'
-import {VStack,StackDivider,InputRightElement,Input,InputGroup,Box,FormControl, FormLabel,Button} from '@chakra-ui/react';
+import {VStack,StackDivider,useToast,InputRightElement,Input,InputGroup,Box,FormControl, FormLabel,Button} from '@chakra-ui/react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom'
 
 const Login = () => {
 	const[show,setShow]=useState(false)
 	const[pass,setPass]=useState()
-	const[email,setEmail]=useState()
+	const[email,setEmail]=useState();
+	const [loading,setLoading]=useState(false);
+	const toast=useToast();
+	const history=useHistory()
 
 	const handleClick=()=>{
 		setShow(!show)
@@ -14,7 +19,52 @@ const Login = () => {
 
 	}
 	
-	const submitHandler=()=>{
+	const submitHandler=async ()=>{
+		setLoading(true);
+		debugger
+
+		if(!email || !pass){
+			toast({
+				title:"Please fill all fields",
+				status:"warnin g",
+				duration:5000,
+				isClosable:true,
+				position:"bottom"
+			});
+			setLoading(false);
+			return;
+		}
+
+		try{
+			const config={
+				headers:{
+					'Content-type':"application/json"
+				}
+			};
+			const {data}=await axios.post("/api/user/login",{email,pass},config
+			);
+			toast({
+				title:"Login successful",
+				status:"success",
+				duration:5000,
+				isClosable:true,
+				position:"bottom"
+			});
+			localStorage.setItem("userInfo",JSON.stringify(data));
+			setLoading(false);
+			history.push('/chats')
+		}
+		catch(err){
+			toast({
+				title:"Error occured",
+				description:err.response.data.message,
+				status:"error",
+				duration:5000,
+				isClosable:true,
+				position:"bottom"
+			});
+			setLoading(false);
+		}
 
 	}
 
@@ -31,6 +81,7 @@ const Login = () => {
 		<Input
 			placeholder='Enter your email'
 			onChange={(e)=>{setEmail(e.target.value)}}
+			value={email}
 			>
 			</Input>
 	</FormControl> 
@@ -43,6 +94,7 @@ const Login = () => {
 			type={show? "text":"password"}
 			placeholder='Enter your password'
 			onChange={(e)=>{setPass(e.target.value)}}
+			value={pass}
 			/>
 		<InputRightElement>
 		<Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -50,28 +102,29 @@ const Login = () => {
 		</Button>
 		</InputRightElement>
 		</InputGroup>
-	</FormControl> 
-	<Button
-	colorScheme='blue'
-	width="100%"
-	style={{marginTop:15}}
-	onClick={submitHandler}	
-	>
-		Login
-	</Button>
-	<Button
-	variant="solid"
-	colorScheme="red"
-	width="100%"
-	style={{marginTop:15}}
-	onClick={()=>{
-		setEmail("guest@hamza.com");
-		setPass("Test*1234")
-	}}	
-	>
-		Get guest credentials
-	</Button>
-</VStack>
+		</FormControl> 
+		<Button
+		colorScheme='blue'
+		width="100%"
+		style={{marginTop:15}}
+		onClick={submitHandler}	
+		isLoading={loading}
+		>
+			Login 
+		</Button>
+		<Button
+		variant="solid"
+		colorScheme="red"
+		width="100%"
+		style={{marginTop:15}}
+		onClick={()=>{
+			setEmail("guest@hamza.com");
+			setPass("Test*1234")
+		}}	
+		>
+			Get guest credentials
+		</Button>
+	</VStack>
   )
 }
 
